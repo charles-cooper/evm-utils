af5f064bb27de2609f3f8a85f026698f1900c4ff
Charles Cooper <cooper.charles.m@gmail.com>
2019-11-27 16:20:23 -0800

    fix the comments in run_vyper

--- run_vyper.sh
+++ run_vyper.sh
@@ -3,9 +3,7 @@
 set -Eeuxo pipefail
 file="$1"
 vyper -f bytecode_runtime "${file}" \
-  # strip leading 0x
-  | tail -c+3 \
-  # strip trailing newline
-  | head -c-1 \
+  | tail -c+3 `# strip leading 0x` \
+  | head -c-1 `# strip trailing newline` \
   > run.evm
 evm --codefile run.evm --input $2 --debug run
