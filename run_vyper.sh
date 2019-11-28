2dc38ce2bc40ef9af53e0452f385e627fe2b8657
Charles Cooper <cooper.charles.m@gmail.com>
2019-11-27 16:03:41 -0800

    add some comments to run_vyper

--- run_vyper.sh
+++ run_vyper.sh
@@ -2,5 +2,10 @@
 # compile and run an LLL file
 set -Eeuxo pipefail
 file="$1"
-vyper -f bytecode "${file}" | tail -c+3 | head -c-1 > run.evm
+vyper -f bytecode_runtime "${file}" \
+  # strip leading 0x
+  | tail -c+3 \
+  # strip trailing newline
+  | head -c-1 \
+  > run.evm
 evm --codefile run.evm --input $2 --debug run
